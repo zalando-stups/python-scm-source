020aa349ceae2cd304495509980876cadf15b19d
Henning Jacobs <henning@jacobs1.de>
2016-07-01 16:58:31 +0200

    #1 test arg parsing too

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,3 +7,20 @@ def test_main(monkeypatch):
     runner = CliRunner()
     result = runner.invoke(main, [], catch_exceptions=False)
     assert 'Generating scm-source.json.. LOCALLY MODIFIED' in result.output
+
+
+def test_directory_arg(monkeypatch):
+    def generate_scm_source(x, y, directory):
+        assert 'mydir' == directory
+        return False
+    monkeypatch.setattr('scm_source.cli.generate_scm_source', generate_scm_source)
+    runner = CliRunner()
+    result = runner.invoke(main, ['mydir'], catch_exceptions=False)
+    assert 'Generating scm-source.json.. OK' in result.output
+
+
+def test_directory_multiple_args(monkeypatch):
+    runner = CliRunner()
+    result = runner.invoke(main, ['foo', 'bar'], catch_exceptions=False)
+    assert 'At most one directory can be given' in result.output
+    assert 2 == result.exit_code
